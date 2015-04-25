0f2d5d6de5e8d8afcee7145f8e3c1cccd4b10d8c
ks56 <ks56@newegg.com>
2015-04-25 17:35:19 +0800

    Update

---
+++ NET/Utilities/Serialization/Serialization.Xml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Utilities.Serialization
+{
+    public static partial class Serialization
+    {
+        public static string XmlSerialize<T>(T serialObject)
+        {
+            StringBuilder sb = new StringBuilder();
+            XmlSerializer ser = new XmlSerializer(typeof(T));
+            using (TextWriter writer = new StringWriter(sb))
+            {
+                ser.Serialize(writer, serialObject);
+                return writer.ToString();
+            }
+        }
+
+        public static string XmlSerialize(object serialObject)
+        {
+            StringBuilder sb = new StringBuilder(5000);
+            XmlSerializer ser = new XmlSerializer(serialObject.GetType());
+            using (TextWriter writer = new StringWriter(sb))
+            {
+                ser.Serialize(writer, serialObject);
+                return writer.ToString();
+            }
+        }
+
+        public static T XmlDeserialize<T>(string str)
+        {
+            XmlSerializer mySerializer = new XmlSerializer(typeof(T));
+            using (TextReader reader = new StringReader(str))
+            {
+                return (T)mySerializer.Deserialize(reader);
+            }
+        }
+
+        public static object XmlDeserialize(string str, Type type)
+        {
+            XmlSerializer mySerializer = new XmlSerializer(type);
+            using (TextReader reader = new StringReader(str))
+            {
+                return mySerializer.Deserialize(reader);
+            }
+        }
+    }
+}
---
+++ NET/Utilities/UnitTest/Extensions/Extension.Tester.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Utilities.Extensions;
+
+namespace Utilities.UnitTest.Extensions
+{
+    [TestFixture]
+    public class ExtensionTester
+    {
+        [Test]
+        public void UnitTest_Paging()
+        {
+            foreach (var list in new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }.Paging(5))
+            {
+                Console.WriteLine("******");
+                foreach (var i in list)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
