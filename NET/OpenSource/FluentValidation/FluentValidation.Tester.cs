402c75037f35a150e2133ba8ac9fc8ec16da650c
ks56 <ks56@newegg.com>
2015-04-28 18:13:42 +0800

    调整结构

---
+++ NET/OpenSource/FluentValidation/FluentValidation.Tester.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using NUnit.Framework;
+using OpenSource.Model;
+
+namespace OpenSource.FluentValidation
+{
+    [TestFixture]
+    public class FluentValidationTester
+    {
+        [Test]
+        public void UnitTest_CallValidate()
+        {
+            var item = new Item { ItemNumber = "SI001", Price = 0, Title = "12" };
+            IValidator<Item> validator = new ItemValidator();
+            var results = validator.Validate(item);
+
+            if (!results.IsValid)
+                Console.WriteLine(string.Join(",", results.Errors.Select(error => error.ErrorMessage)));
+
+            Assert.IsTrue(results.IsValid);
+        }
+    }
+}
---
+++ NET/OpenSource/FluentValidation/ItemValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using OpenSource.Model;
+
+namespace OpenSource.FluentValidation
+{
+    public class ItemValidator : AbstractValidator<Item>
+    {
+        public ItemValidator()
+        {
+            RuleFor(item => item.ItemNumber)
+                .NotEmpty()
+                .Length(5, 10)
+                .WithName("9SI");
+            RuleFor(item => item.Title).NotEmpty().Length(2 /* 含包min */, 10 /* 含包max */);
+            RuleFor(item => item.Price).ExclusiveBetween(0 /* 不含包from */, 100 /* 不含包to */);
+        }
+    }
+}
